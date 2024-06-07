56a212aeab1be09bdb69bbc7354f77df8cdd7b93
Seba <seba.penah@gmail.com>
2024-06-07 15:31:07 -0400

    Agregando Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <header className='d-flex justify-content-center bg-success p-3'>
+        <h1 className='text-white'>Desafio 2 - Estado de los Componentes</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Header
