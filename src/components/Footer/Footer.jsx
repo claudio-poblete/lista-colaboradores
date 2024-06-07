f9bea695134cc800586f27961368f061bb3e0cca
Seba <seba.penah@gmail.com>
2024-06-07 15:32:39 -0400

    Agregando Header

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className='d-flex justify-content-center bg-success p-4'>
+      <strong className='text-white'> &copy; 2024 Academia DesafioLatam. Hecho con fines académicos. </strong>
+    </div>
+  )
+}
+
+export default Footer
